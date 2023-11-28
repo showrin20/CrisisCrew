2697c3beb9f9ac293f8d0526a346a050d4f9d691
showrin20 <showrin.rahman@g.bracu.ac.bd>
2023-11-28 16:16:04 +0600

    first commit

--- myscript.js
+++ myscript.js
@@ -1,34 +1,34 @@
-$(function(){
-    $('.profile_container').on('click', function(e){
-      var $biginfo = $('#teamcontent');
-      var $bigname = $('#bigname');
-      var $bigjob  = $('#bigjob');
-      var $bigdesc = $('#bigdesc');
-      var $bigimg  = $('.bigimg');  
-      
-      var newimg  = $(this).find('.profilepic').attr('src');
-      var newname = $(this).find('.profilepic').attr('alt');
-      var newrole = $(this).siblings('.job').eq(0).html();
-      var newdesc = $(this).siblings('.desc').eq(0).html();
-      
-      $bigimg.css('background-image', "url(" + newimg + ")");
-      $bigname.html(newname);
-      $bigjob.html(newrole);
-      $bigdesc.html(newdesc);
-      
-      if($biginfo.css('display') == 'none') {
-        $biginfo.slideDown(350);
-      }
-        
-      $('html, body').animate({ scrollTop: $('.team_box').offset().top }, 'slow');  
-    });
+$(function () {
+  $(".profile_container").on("click", function (e) {
+    var $biginfo = $("#teamcontent");
+    var $bigname = $("#bigname");
+    var $bigjob = $("#bigjob");
+    var $bigdesc = $("#bigdesc");
+    var $bigimg = $(".bigimg");
+
+    var newimg = $(this).find(".profilepic").attr("src");
+    var newname = $(this).find(".profilepic").attr("alt");
+    var newrole = $(this).siblings(".job").eq(0).html();
+    var newdesc = $(this).siblings(".desc").eq(0).html();
+
+    $bigimg.css("background-image", "url(" + newimg + ")");
+    $bigname.html(newname);
+    $bigjob.html(newrole);
+    $bigdesc.html(newdesc);
+
+    if ($biginfo.css("display") == "none") {
+      $biginfo.slideDown(350);
+    }
+
+    $("html, body").animate({ scrollTop: $(".team_box").offset().top }, "slow");
   });
-      
-  function biograph(e) {
-      if(e.className == 'about__slot') {
-          e.className = 'about__slot bio';
-      }
-      else {
-          e.className = 'about__slot';
-      }
+});
+
+function biograph(e) {
+  if (e.className == "about__slot") {
+    e.className = "about__slot bio";
+  } else {
+    e.className = "about__slot";
   }
+}
+
